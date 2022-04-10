2f278bc6b45aa98dc2b3e00012139f47298517cb
Amir Tavakoli <atavakoli@travix.com>
2022-04-10 18:17:17 +0200

    initial commit

---
+++ src/main/java/com/apm/demo/demo/ApmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.apm.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apm/demo/demo/ApmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apm.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
